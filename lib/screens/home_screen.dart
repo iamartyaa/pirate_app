42d0e06e40ed5add112eb2409cb60d0544135e18
Amartya Yadav <iamartyayadav@gmail.com>
2022-05-13 21:02:44 +0530

    modified Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pirate_app/widgets/badge.dart';
+import 'package:pirate_app/widgets/body.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName='/home-screen';
@@ -6,9 +8,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pirate land'),
+        title: Text('Pirate Island'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search),),
+          Badge(
+              child: IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                onPressed: () {
+                  
+                },
+              ),
+              value: 0.toString(),
+              color: Colors.green),
+        ],
       ),
-      body: Container(),
+      body: Body(),
     );
   }
 }
