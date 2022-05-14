a1d029dd75583d2a7d08ad5651c061822c6cac5d
Amartya Yadav <iamartyayadav@gmail.com>
2022-05-14 12:59:10 +0530

    addToCart feature

--- lib/widgets/add_cart.dart
+++ lib/widgets/add_cart.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pirate_app/providers/cart.dart';
+import 'package:provider/provider.dart';
 import '../providers/product.dart';
+
 
 class AddCart extends StatelessWidget {
   final Product product;
-
+  // final String s;
+  // final int quan;
   AddCart({required this.product});
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: Row(
@@ -27,7 +32,23 @@ class AddCart extends StatelessWidget {
                 Icons.shopping_cart,
                 color: product.color,
               ),
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.price, product.title);//error prone
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text(
+                      'Added item to Cart',
+                    ),
+                    duration: const Duration(seconds: 2),
+                    action: SnackBarAction(
+                        label: 'UNDO',
+                        onPressed: () {
+                          cart.removeSingleItem(product.id);
+                        }),
+                  ),
+                );
+              },
             ),
           ),
           Expanded(
