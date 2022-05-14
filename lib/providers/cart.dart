e82dee7c77639aa8a8185ab9f1edb8385103e391
Amartya Yadav <iamartyayadav@gmail.com>
2022-05-14 12:08:51 +0530

    added cart products widget

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -6,7 +6,6 @@ class CartItem {
   final int quantity;
   final double price;
   final String size;
-  final String color;
 
   CartItem({
     required this.id,
@@ -14,6 +13,83 @@ class CartItem {
     required this.quantity,
     required this.price,
     required this.size,
-    required this.color,
   });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cart) {
+      total += cart.price * cart.quantity;
+    });
+    return total;
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (value) => CartItem(
+          id: value.id,
+          title: value.title,
+          quantity: value.quantity + 1,
+          price: value.price,
+          size: value.size,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          quantity: 1,
+          price: price,
+          size: "S",
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (item) => CartItem(
+          id: item.id,
+          title: item.title,
+          quantity: item.quantity - 1,
+          price: item.price,
+          size: item.size,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
 }
---
+++ lib/widgets/cart_products.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:pirate_app/providers/cart.dart';
+import 'package:provider/provider.dart';
+
+class CartProducts extends StatelessWidget {
+  final String id;
+  final String productId;
+  final double price;
+  final int quantity;
+  final String title;
+  final String size;
+  CartProducts({
+    required this.id,
+    required this.productId,
+    required this.price,
+    required this.quantity,
+    required this.title,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Are you Sure?'),
+            content:
+                const Text('Do you want to remove the Item from the Cart?'),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text(
+                  'NO',
+                  style: TextStyle(color: Colors.deepOrange),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text(
+                  'YES',
+                  style: TextStyle(color: Colors.deepOrange),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+      ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(child: Text('\$ ${price}')),
+              ),
+            ),
+            title: Text(
+              title,
+              style: const TextStyle(color: Colors.black54),
+            ),
+            subtitle: Text('Total: \$ ${price * quantity}'),
+            trailing: Text('$quantity x'),
+          ),
+        ),
+      ),
+    );
+  }
+}
